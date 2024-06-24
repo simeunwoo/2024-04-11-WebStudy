18acc00c659b21925a1603e3b31cc900a74465c6
sist115_1 <sist115_1@sist115>
2024-06-24 15:45:16 +0900

    0624

---
+++ HTMLProject_1/src/main/webapp/html/js.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */	function start1(){
+		alert("자바스크립트 시작1");
+	}
+	let start2=function(){
+		alert("자바스크립트 시작2");
+	}
+	// 자바스크립트 6버전에서 주로 사용 => Arrow 함수 => function, return을 제거하여 사용
+	// 자바 => 람다식
+	let start3=()=>{
+		alert("자바스크립트 시작3");
+	}
