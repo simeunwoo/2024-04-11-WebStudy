0813aca2f8a6403c6fcf9bc1f9ae666a416b3a95
sist115_1 <sist115_1@sist115>
2024-06-27 16:46:58 +0900

    0627

--- HTMLCSSProject/src/main/java/com/sist/dao/MusicVO.java
+++ HTMLCSSProject/src/main/java/com/sist/dao/MusicVO.java
@@ -1,19 +1,20 @@
 package com.sist.dao;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /*
- MNO                                       NOT NULL NUMBER
- TITLE                                     NOT NULL VARCHAR2(2000)
- SINGER                                    NOT NULL VARCHAR2(2000)
- ALBUM                                              VARCHAR2(2000)
- POSTER                                    NOT NULL VARCHAR2(300)
- STATE                                              CHAR(12)
- IDCREMENT                                          NUMBER
+ *  MNO                                       NOT NULL NUMBER
+    TITLE                                     NOT NULL VARCHAR2(2000)
+    SINGER                                    NOT NULL VARCHAR2(2000)
+    ALBUM                                              VARCHAR2(2000)
+    POSTER                                    NOT NULL VARCHAR2(300)
+    STATE                                              CHAR(12)
+    IDCREMENT                                          NUMBER
  */
-@Data
+@Getter
+@Setter
 public class MusicVO {
-	private int mno,idcrement;
-	private String title,singer,album,poster,state;
-	
+    private int mno,idcrement;
+    private String title,singer,album,poster,state;
 }
