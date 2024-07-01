b8e1f76d8ed0cb307e70ee6fe3f9b1cf3171d3c4
sist115_1 <sist115_1@sist115>
2024-07-01 10:29:29 +0900

    0701

--- HTMLCSSProject_3/src/main/java/com/sist/dao/FoodVO.java
+++ HTMLCSSProject_3/src/main/java/com/sist/dao/FoodVO.java
@@ -17,4 +17,77 @@ public class FoodVO {
 	private int fno,hit,jjimcount,likecount;
 	private double score;
 	private String name,type,phone,address,theme,poster,content;
+	public int getFno() {
+		return fno;
+	}
+	public void setFno(int fno) {
+		this.fno = fno;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getJjimcount() {
+		return jjimcount;
+	}
+	public void setJjimcount(int jjimcount) {
+		this.jjimcount = jjimcount;
+	}
+	public int getLikecount() {
+		return likecount;
+	}
+	public void setLikecount(int likecount) {
+		this.likecount = likecount;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
