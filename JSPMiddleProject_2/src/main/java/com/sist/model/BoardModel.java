feac7a1bf000b6728c5e3edb90bb67c1440d06c0
sist115_1 <sist115_1@sist115>
2024-07-19 09:52:28 +0900

    0719

---
+++ JSPMiddleProject_2/src/main/java/com/sist/model/BoardModel.java
@@ -0,0 +1,17 @@
+package com.sist.model;
+// 요청 처리 후에 결과값을 JSP로 전송 : Model
+// 오라클 연결 / 데이터를 모은다 / 전송
+//   DAO         VO      Model => Model(Back-End)
+/*
+ * 	1. GIT
+ * 	2. 데이터베이스 설계 => 정규화
+ * 	3. ERD
+ * 	4. 유스케이스 다이어그램
+ * 	5. UI => 화면
+ * 	6. 구현
+ * 	7. 테스팅
+ * 	8. 배포 => AWS
+ */
+public class BoardModel {
+
+}
