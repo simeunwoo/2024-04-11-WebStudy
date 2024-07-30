660e69c76a552b374f52aff540d4129b0838f480
sist115_1 <sist115_1@sist115>
2024-07-30 15:35:54 +0900

    0730

--- JavaScriptProject_3/src/main/java/com/sist/model/FoodModel.java
+++ JavaScriptProject_3/src/main/java/com/sist/model/FoodModel.java
@@ -1,8 +1,12 @@
 package com.sist.model;
+import java.io.PrintWriter;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.sist.controller.RequestMapping;
 import com.sist.dao.*;
@@ -39,6 +43,11 @@ public class FoodModel {
 	@RequestMapping("js/food_find_js.do") // _js : javascript에서 데이터를 읽어 간다 => JSON 제작
 	public void food_find_js(HttpServletRequest request,HttpServletResponse response)
 	{
+		try
+		{
+			request.setCharacterEncoding("UTF-8");
+		}catch(Exception ex) {}
+		
 		String page=request.getParameter("page");
 		if(page==null)
 			page="1";
@@ -47,11 +56,67 @@ public class FoodModel {
 			addr="마포";
 		
 		int curpage=Integer.parseInt(page);
+		List<FoodVO> list=dao.foodFindData(curpage, addr);
+		int totalpage=dao.foodFindTotalPage(addr);
+		
+		final int BLOCK=10;
+		int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+		int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+		
+		if(endPage>totalpage)
+			endPage=totalpage;
+		
+		int i=0;
+		JSONArray arr=new JSONArray(); // List => restful
+		for(FoodVO vo:list)
+		{
+			JSONObject obj=new JSONObject(); // VO
+			
+			obj.put("fno", vo.getFno());
+			obj.put("name", vo.getName());
+			obj.put("poster", vo.getPoster().replace("https", "http"));
+			obj.put("address", vo.getAddress());
+			
+			if(i==0)
+			{
+				obj.put("curpage", curpage);
+				obj.put("totalpage", totalpage);
+				obj.put("startPage", startPage);
+				obj.put("endPage", endPage);
+			}
+			arr.add(obj);
+			i++;
+		}
+		
+		try
+		{
+			response.setContentType("text/plain;charset=UTF-8");
+			// html => json
+			PrintWriter out=response.getWriter();
+			out.print(arr.toJSONString());
+			out.close();
+		}catch(Exception ex) {ex.printStackTrace();}
 	}
 	
 	@RequestMapping("js/food_detail_js.do")
 	public void food_detail_js(HttpServletRequest request,HttpServletResponse response)
 	{
+		String fno=request.getParameter("fno");
+		FoodVO vo=dao.foodDetailData(Integer.parseInt(fno));
 		
+		JSONObject obj=new JSONObject();
+		obj.put("name", vo.getName());
+		obj.put("address", vo.getAddress());
+		obj.put("phone", vo.getPhone());
+		obj.put("score", vo.getScore());
+		obj.put("theme", vo.getTheme());
+		obj.put("poster", vo.getPoster().replace("https", "http"));
+		
+		try
+		{
+			response.setContentType("text/plain;charset=UTF-8");
+			PrintWriter out=response.getWriter();
+			out.print(obj.toJSONString());
+		}catch(Exception ex) {}
 	}
 }
