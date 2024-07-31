c9bd4d4f3b2e12ebf3100d2bb1c10e72f1e1586a
sist115_1 <sist115_1@sist115>
2024-07-31 09:09:34 +0900

    0731

---
+++ JavaScriptProject_4/src/main/java/com/sist/dao/EmpVO.java
@@ -0,0 +1,9 @@
+package com.sist.dao;
+import java.util.*;
+// => MyBatis : 컬럼명과 일치 => 변경
+public class EmpVO {
+
+	private int empno,sal,comm,deptno,mgr;
+	private String ename,job;
+	private Date hiredate;
+}
