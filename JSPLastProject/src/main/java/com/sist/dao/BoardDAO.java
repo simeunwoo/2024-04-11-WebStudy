96d63479e58590c05f375694262efabc236fe38d
sist115_1 <sist115_1@sist115>
2024-08-07 09:50:19 +0900

    0807

--- JSPLastProject/src/main/java/com/sist/dao/BoardDAO.java
+++ JSPLastProject/src/main/java/com/sist/dao/BoardDAO.java
@@ -1,5 +1,83 @@
 package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.sist.vo.BoardVO;
 
 public class BoardDAO {
 
+	private static SqlSessionFactory ssf;
+	static
+	{
+		ssf=CreateSqlSessionFactory.getSsf();
+	}
+	
+	/*
+	<insert id="boardInsert" parameterType="BoardVO">
+		<selectKey keyProperty="no" resultType="int" order="BEFORE">
+			SELECT NVL(MAX(no)+1,1) as no
+			FROM project_board
+		</selectKey>
+		INSERT INTO project_board(no,name,subject,content,pwd)
+		VALUES(#{no},#{name},#{subject},#{content},#{pwd})
+	</insert>
+	 */
+	public static void boardInsert(BoardVO vo)
+	{
+		SqlSession session=null;
+		
+		try
+		{
+			// 연결 => Connection 주소 얻기 => DBCP : 미리 Connection 객체를 여러개 생성
+			// Connection => 오라클 연결 시간이 오래 걸린다
+			// 장점 : 미리 만들면 연결에 소모되는 시간을 줄일 수 있다, Connection 객체를 조절할 수 있다
+			// openSession() => autoCommit(false) : 자동 커밋 X => 저장이 안됨
+			// => openSession(true) => autoCommit(true) : 자동 커밋 => 저장이 됨
+			session=ssf.openSession(true);
+			session.insert("boardInsert",vo);
+		}catch(Exception ex)
+		{
+			System.out.println("BoardDAO 오류 1");
+			ex.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				session.close(); // 반환 (DBCP 사용)
+		}
+	}
+	
+	/*
+	<select id="boardListData" resultType="BoardVO" parameterType="hashmap">
+		SELECT no,subject,name,TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit,num
+		FROM (SELECT no,subject,name,regdate,hit,rownum as num
+		FROM (SELECT no,subject,name,regdate,hit
+		FROM project_board))
+		WHERE num BETWEEN #{start} AND #{end}
+	</select>
+	 */
+	public static List<BoardVO> boardListData(Map map)
+	{
+		List<BoardVO> list=new ArrayList<BoardVO>();
+		SqlSession session=null;
+		
+		try
+		{
+			session=ssf.openSession();
+			list=session.selectOne("boardListData",map);
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("BoardDAO 오류 2");
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				session.close();
+		}
+		
+		return list;
+	}
 }
--- JSPLastProject/src/main/java/com/sist/vo/WordVO.java
+++ JSPLastProject/src/main/java/com/sist/vo/WordVO.java
@@ -1,5 +1,10 @@
 package com.sist.vo;
 
+import lombok.Data;
+
+@Data
 public class WordVO {
 
+	private String word;
+	private int count;
 }
