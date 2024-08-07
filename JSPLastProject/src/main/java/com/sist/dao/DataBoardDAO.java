4ba39f94e679526a8d23d7c5fb9eee0cd34fd67d
sist115_1 <sist115_1@sist115>
2024-08-07 16:19:58 +0900

    0807

--- JSPLastProject/src/main/java/com/sist/dao/DataBoardDAO.java
+++ JSPLastProject/src/main/java/com/sist/dao/DataBoardDAO.java
@@ -1,6 +1,7 @@
 package com.sist.dao;
 import java.util.*;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.sist.vo.*;
@@ -13,6 +14,69 @@ public class DataBoardDAO {
 		ssf=CreateSqlSessionFactory.getSsf();
 	}
 	
-	// 기능
+	public static void databoardInsert(DataBoardVO vo)
+	{
+		SqlSession session=null;
+		
+		try
+		{
+			session=ssf.openSession();
+			session.insert("databoardInsert",vo);
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("BoardDAO 오류 1");
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				session.close();
+		}
+	}
 	
+	public static List<DataBoardVO> databoardListData(Map map)
+	{
+		List<DataBoardVO> list=new ArrayList<DataBoardVO>();
+		SqlSession session=null;
+		
+		try
+		{
+			session=ssf.openSession();
+			list=session.selectList("databoardListData",map);
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("BoardDAO 오류 2");
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				session.close();
+		}
+		
+		return list;
+	}
+	
+	public static int databoardRowCount()
+	{
+		int count=0;
+		SqlSession session=null;
+		
+		try
+		{
+			session=ssf.openSession();
+			count=session.selectOne("databoardRowCount");
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("BoardDAO 오류 2");
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				session.close();
+		}
+		
+		return count;
+	}
 }
--- JSPLastProject/src/main/java/com/sist/model/DataBoardModel.java
+++ JSPLastProject/src/main/java/com/sist/model/DataBoardModel.java
@@ -1,5 +1,53 @@
 package com.sist.model;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.vo.*;
+import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
 
 public class DataBoardModel {
 
+	@RequestMapping("databoard/insert.do")
+	public String databoard_insert(HttpServletRequest request,HttpServletResponse response)
+	{
+		request.setAttribute("main_jsp", "../databoard/insert.jsp");
+		return "../main/main.jsp";
+	}
+	
+	@RequestMapping("databoard/insert_ok.do")
+	public String databoard_insert_ok(HttpServletRequest request,HttpServletResponse response)
+	{
+		return "redirect:../databoard/list.do";
+	}
+	
+	@RequestMapping("databoard/list.do")
+	public String databoard_list(HttpServletRequest request,HttpServletResponse response)
+	{
+		String page=request.getParameter("page");
+		if(page==null)
+			page="1";
+		int curpage=Integer.parseInt(page);
+		
+		Map map=new HashMap();
+		map.put("start", (curpage*10)-9);
+		map.put("end", curpage*10);
+		
+		//////////////////////////////////////////////////////////////////////
+		
+		List<DataBoardVO> list=DataBoardDAO.databoardListData(map);
+		int count=DataBoardDAO.databoardRowCount();
+		int totalpage=(int)(Math.ceil(count/10.0));
+		count=count-((curpage*10)-10);
+		
+		request.setAttribute("list", list);
+		request.setAttribute("count", count);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		
+		request.setAttribute("main_jsp", "../databoard/list.jsp");
+		return "../main/main.jsp";
+	}
 }
