701a4f2c6e8aeb9bb8dd9be56772088d49ac210e
sist115_1 <sist115_1@sist115>
2024-08-07 15:29:14 +0900

    0807

--- JSPLastProject/src/main/java/com/sist/dao/DataBoardDAO.java
+++ JSPLastProject/src/main/java/com/sist/dao/DataBoardDAO.java
@@ -1,5 +1,18 @@
 package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.sist.vo.*;
 
 public class DataBoardDAO {
 
+	private static SqlSessionFactory ssf;
+	static
+	{
+		ssf=CreateSqlSessionFactory.getSsf();
+	}
+	
+	// 기능
+	
 }
