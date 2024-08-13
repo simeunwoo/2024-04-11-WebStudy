fd0d7b64bbdb3a53452dfc104ee9e3fa8b386e45
sist115_1 <sist115_1@sist115>
2024-08-13 14:50:00 +0900

    0813

--- JSPLastProject/src/main/java/com/sist/model/AllJjimModel.java
+++ JSPLastProject/src/main/java/com/sist/model/AllJjimModel.java
@@ -1,5 +1,46 @@
 package com.sist.model;
+import java.io.PrintWriter;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sist.vo.*;
+import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
 
 public class AllJjimModel {
 
+	@RequestMapping("all_jjim/insert.do")
+	public void all_jjim_insert(HttpServletRequest request,HttpServletResponse response)
+	{
+		String cno=request.getParameter("cno");
+		String type=request.getParameter("type");
+		
+		HttpSession session=request.getSession();
+		String id=(String)session.getAttribute("id");
+		
+		Map map=new HashMap();
+		map.put("cno", cno);
+		map.put("type", type);
+		map.put("id", id);
+		
+		String result="";
+		try
+		{
+			AllJjimDAO.allJjimInsert(map);
+			result="OK";
+		}catch(Exception ex)
+		{
+			result=ex.getMessage();
+		}
+		
+		// Ajax로 전송
+		try
+		{
+			PrintWriter out=response.getWriter();
+			out.write(result);
+		}catch(Exception ex) {}
+	}
 }
