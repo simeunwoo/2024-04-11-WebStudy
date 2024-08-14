132c7b068633dc91597ea5cf4bc4751a4f9abc4c
sist115_1 <sist115_1@sist115>
2024-08-14 09:09:05 +0900

    0814

---
+++ JSPLastProject/src/main/java/com/sist/vo/NoticeVO.java
@@ -0,0 +1,22 @@
+package com.sist.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/*
+NO      NOT NULL NUMBER         
+TYPE             NUMBER         
+NAME             VARCHAR2(50)   
+SUBJECT          VARCHAR2(2000) 
+CONTENT          CLOB           
+REGDATE          DATE           
+HIT              NUMBER 
+ */
+@Data
+public class NoticeVO {
+
+	private int no,type,hit;
+	private String name,subject,content,dbday;
+	private Date regdate;
+}
