c7aaac1f7e6fd655ea1c465360198dc129d75dfd
sist115_1 <sist115_1@sist115>
2024-08-21 09:23:50 +0900

    0821

--- JSPLastProject/src/main/java/com/sist/vo/ChefVO.java
+++ JSPLastProject/src/main/java/com/sist/vo/ChefVO.java
@@ -1,4 +1,7 @@
 package com.sist.vo;
+
+import lombok.Data;
+
 /*
 CHEF      NOT NULL VARCHAR2(100) 
 POSTER             VARCHAR2(500) 
@@ -7,6 +10,8 @@ MEM_CONT3          VARCHAR2(100)
 MEM_CONT7          VARCHAR2(100) 
 MEM_CONT2          VARCHAR2(100) 
  */
+@Data
 public class ChefVO {
 
+	private String chef,poster,men_con1,men_con3,men_con7,men_con2;
 }
--- JSPLastProject/src/main/java/com/sist/vo/RecipeVO.java
+++ JSPLastProject/src/main/java/com/sist/vo/RecipeVO.java
@@ -1,4 +1,7 @@
 package com.sist.vo;
+
+import lombok.Data;
+
 /*
 NO     NOT NULL NUMBER         
 TITLE  NOT NULL VARCHAR2(1000) 
@@ -7,6 +10,9 @@ CHEF   NOT NULL VARCHAR2(100)
 LINK            VARCHAR2(500)  
 HIT             NUMBER      
  */
+@Data
 public class RecipeVO {
 
+	private int no,hit;
+	private String title,poster,chef,link;
 }
