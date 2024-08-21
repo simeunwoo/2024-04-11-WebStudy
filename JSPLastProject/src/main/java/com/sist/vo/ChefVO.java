042c77f43e6b472f4858534cae1c1c048178b44c
sist115_1 <sist115_1@sist115>
2024-08-21 09:16:36 +0900

    0821

---
+++ JSPLastProject/src/main/java/com/sist/vo/ChefVO.java
@@ -0,0 +1,12 @@
+package com.sist.vo;
+/*
+CHEF      NOT NULL VARCHAR2(100) 
+POSTER             VARCHAR2(500) 
+MEM_CONT1          VARCHAR2(100) 
+MEM_CONT3          VARCHAR2(100) 
+MEM_CONT7          VARCHAR2(100) 
+MEM_CONT2          VARCHAR2(100) 
+ */
+public class ChefVO {
+
+}
---
+++ JSPLastProject/src/main/java/com/sist/vo/RecipeVO.java
@@ -0,0 +1,12 @@
+package com.sist.vo;
+/*
+NO     NOT NULL NUMBER         
+TITLE  NOT NULL VARCHAR2(1000) 
+POSTER NOT NULL VARCHAR2(500)  
+CHEF   NOT NULL VARCHAR2(100)  
+LINK            VARCHAR2(500)  
+HIT             NUMBER      
+ */
+public class RecipeVO {
+
+}
